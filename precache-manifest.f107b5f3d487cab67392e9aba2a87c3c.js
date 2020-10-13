8318d038ab15949df228712e8179b1fe54025f65
Eric Farnan <ericfarnan94@gmail.com>
2020-10-13 11:14:36 -0500

    Updates

---
+++ precache-manifest.f107b5f3d487cab67392e9aba2a87c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99650647cdd1d22807dfa66fe37f65b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "726ff006c8107092387d",
+    "url": "/monsters-rolodex/static/css/main.6244c80e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726ff006c8107092387d",
+    "url": "/monsters-rolodex/static/js/main.ef7cad35.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
